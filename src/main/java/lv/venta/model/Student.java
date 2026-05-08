8fabff25a6b37368c6e40273df8cefacb91e8ae8
buraaksen <buraaksen@outlook.com>
2026-05-08 15:32:13 +0300

    first

---
+++ src/main/java/lv/venta/model/Student.java
@@ -0,0 +1,66 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Table(name = "StudentTable")
+@Entity()
+public class Student {
+	
+	@Column(name = "Sid")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int sid; 
+	
+	@Column(name = "Name")
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Z]{1}[a-z]{2,40}")
+	private String name; 
+	
+	@Column(name = "Surname")
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Z]{1}[a-z]{2,40}")
+	private String surname;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getSid() {
+		return sid;
+	}
+	
+	
+	public Student() {}
+	
+	public Student (String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
+	
+	public String toString() {
+		return sid+ ": "+ name +" " +surname;
+	}
+	
+}
